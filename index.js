3a9761676b585cd225f6043a8731b373f997674c
himanshu999 <himanshunathani999@gmail.com>
2021-01-22 03:04:57 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const gltfPipeline = require('gltf-pipeline');
+const fsExtra = require('fs-extra');
+const glbToGltf = gltfPipeline.glbToGltf;
+//const glb = fsExtra.readFileSync('model.glb');
+
+/*glbToGltf(glb)
+    .then(function(results) {
+        fsExtra.writeJsonSync('model.gltf', results.gltf);
+    }); */
